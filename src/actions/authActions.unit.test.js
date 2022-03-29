4a540b773f68677016659d79ffd6d092fac249ac
Fabian Parra <fabian.parram@usach.cl>
2022-03-29 20:51:41 -0300

    Add unit test for authActions.

---
+++ src/actions/authActions.unit.test.js
@@ -0,0 +1,20 @@
+import {actions} from './authActions'
+
+describe('AuthActions', () => {
+  test('siginDispatch', () => {
+    const dispatch = jest.fn()
+    const {siginDispatch} = actions(dispatch)
+    const entries = {
+      username: 'test-username',
+      name: 'test-name',
+      token: 'test-token'
+    }
+    siginDispatch(entries)
+    
+    const actionExpected = {
+      type: 'sigin',
+      ...entries
+    }
+    expect(dispatch).toHaveBeenCalledWith(actionExpected)
+  })
+})
